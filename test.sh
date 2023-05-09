d4fb7a9208abfd999b5b719e2e6bb87a9384116a
daeun-j <50479265+daeun-j@users.noreply.github.com>
2023-05-08 21:59:08 -0400

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,6 +1,10 @@
 cd data/utils
-python3 run.py -d cifar10 -cn 10 -c 10
+python3 run.py -d cifar10 -cn 10 -c 10 --num_val 1000
 cd ../../
 
-# cd src/server
-# python3 peacoc.py -d cifar10 -m res18 -ge 3 -le 10 -jr 1 -lr 0.1 -mom 0.9 -wd 0.0005 -bs 128 -vg 1 -ab B > ../test.out
+cd src/server
+# don't change ge, le 
+python3 fedavg.py  -d cifar10 -m res18 -ge 20 -le 20 -jr 1 -lr 0.01 -mom 0.9 -wd 0.0005 -bs 512 -vg 1 >  ../avg.out
+python3 fedprox.py -d cifar10 -m res18 -ge 20 -le 20 -jr 1 -lr 0.01 -mom 0.9 -wd 0.0005 -bs 512 -vg 1 > ../prox.out
+python3 peacoc.py -d cifar10 -m res18 -ge 20 -le 20 -jr 1 -lr 0.01 -mom 0.9 -wd 0.0005 -bs 512 -vg 1 > ../pcoc.out
+python3 peacoc_beta.py -d cifar10 -m res18 -ge 20 -le 20 -jr 1 -lr 0.01 -mom 0.9 -wd 0.0005 -bs 512  -vg 1 > ../pcocbeta.out
